9b5d2f6c2d76c65290ae14ccce57b98a4130b89d
yashrajbains <yashrajbains@gmail.com>
2025-11-09 14:03:02 -0800

    updates

--- src/components/paris.jsx
+++ src/components/paris.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // src/components/paris.jsx
 import React, { useState } from 'react';
 import BackButton from './BackButton';
@@ -54,64 +53,3 @@ export default function Paris() {
     </div>
   );
 }
-=======
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import BackButton from './BackButton.jsx';
-
-function Paris() {
-    const navigate = useNavigate();
-
-    const images = [
-        {
-            src: '/images/eiffelTower.jpg',
-            caption: "Uhhh yea...",
-        },
-        {
-            src: '/images/paris1.jpg',
-            caption: "",
-        },
-        {
-            src: '/images/paris2.jpg',
-            caption: "",
-        },
-        {
-            src: '/images/paris3.jpg',
-            caption: "",
-        },
-        {
-            src: '/images/paris4.jpg',
-            caption: "",
-        },
-        {
-            src: '/images/paris5.jpg',
-            caption: "",
-        },
-        {
-            src: '/images/paris6.jpg',
-            caption: "",
-        },
-        // Add more images and captions as needed
-    ];
-
-    const viewImage = (image) => {
-        navigate('/image-view', { state: image });
-    };
-
-    return (
-        <div className="paris-gallery gallery-page" style={{ paddingTop: '800px' }}>
-            <BackButton />
-            <h1>Paris 2022</h1>
-            <div className="image-grid">
-                {images.map((image, index) => (
-                    <div className="image-item" key={index} onClick={() => viewImage(image)}>
-                        <img src={image.src} alt={`Paris Photo ${index + 1}`} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
-
-export default Paris;
->>>>>>> b3f0d64985ac9757ec8c2c339d41d72178af4ea6
