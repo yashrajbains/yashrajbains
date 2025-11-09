bdfd6ae29786dc04a68dbd54ef2a9a99522f047a
yashrajbains <yashrajbains@gmail.com>
2025-11-09 14:08:35 -0800

    updates

--- src/components/mexicoCity.jsx
+++ src/components/mexicoCity.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// src/components/mexicoCity.jsx
 import React, { useState } from 'react';
 import BackButton from './BackButton';
 import Lightbox from './Lightbox';
@@ -51,52 +49,3 @@ export default function MexicoCity() {
     </div>
   );
 }
-=======
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import BackButton from './BackButton.jsx';
-
-function MexicoCity() {
-    const navigate = useNavigate();
-
-    const images = [
-        {
-            src: '/images/cathedral.jpg',
-            caption: '',
-        },
-        {
-            src: '/images/cathedralFlag.jpg',
-            caption: '',
-        },
-        {
-            src: '/images/chinatown.jpg',
-            caption: '',
-        },
-        {
-            src: '/images/street.jpg',
-            caption: '',
-        },
-        // Add more images with `src` and `caption`
-    ];
-
-    const viewImage = (image) => {
-        navigate('/image-view', { state: image });
-    };
-
-    return (
-        <div className="mexico-gallery gallery-page">
-            <BackButton />
-            <h1>Mexico City 2025</h1>
-            <div className="image-grid">
-                {images.map((image, index) => (
-                    <div className="image-item" key={index} onClick={() => viewImage(image)}>
-                        <img src={image.src} alt={`Mexico Photo ${index + 1}`} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
-
-export default MexicoCity;
->>>>>>> b3f0d64985ac9757ec8c2c339d41d72178af4ea6
