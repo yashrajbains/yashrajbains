badc5db24cb92086142e5a37110e2858598d07c0
yashrajbains <yashrajbains@gmail.com>
2025-11-09 14:25:35 -0800

    git git git

--- src/components/stockton.jsx
+++ src/components/stockton.jsx
@@ -37,57 +37,6 @@ export default function Stockton() {
           </div>
         ))}
       </div>
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import BackButton from './BackButton.jsx';
-
-function Stockton() {
-    const navigate = useNavigate();
-
-    const images = [
-        {
-            src: '/images/impalaGarage.jpg',
-            caption: 'I had just purchased my first camera from Ulmer Photo on Pacific Ave. Minolta SRT 101, ' +
-                'previously owned by Benjamin Reddish. Unfortunately, the shutter had a problem of getting stuck and I ' +
-                'returned the camera to Tim after a pitiful attempt to repair it. Looking back, I should have kept' +
-                ' the camera... Anyways, this picture is one of the two pictures I took with that ' +
-                'camera, on top of the parking garage nearest ' +
-                'to the Regal movie theater in downtown Stockton. "That one garage" And yes, that was my car at the time.',
-        },
-        {
-            src: '/images/nic.jpg',
-            caption: 'My good friend Nic poses as a model for our soon-to-drop clothing brand: P.M. (2021)',
-        },
-        {
-            src: '/images/rubin.jpg',
-            caption: 'Rubin (2021)'
-        },
-        // {
-        //     src: '/images/polaroid.png',
-        //     caption: 'My sister took this polaroid of me and an old friend in front of my 5th Stockton home. ' +
-        //         '83 Eldorado was hiiieeeeeemmm. '
-        // },
-        // Add more images and captions as needed
-    ];
-
-    const viewImage = (image) => {
-        navigate('/image-view', { state: image });
-    };
-
-    return (
-        <div className="stockton-gallery gallery-page">
-            <BackButton />
-            <h1>Some pictures from where I grew up. The best of which are Missing.</h1>
-            <div className="image-grid">
-                {images.map((image, index) => (
-                    <div className="image-item" key={index} onClick={() => viewImage(image)}>
-                        <img src={image.src} alt={`Stockton Photo ${index + 1}`} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
 
       {open && active && (
         <Lightbox
