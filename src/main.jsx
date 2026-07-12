052202aed70b0cfe84aa81fca2636ada408a6ae3
yashrajbains <yashrajbains@gmail.com>
2026-07-12 13:23:55 -0700

    Switch to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import Analytics from './components/Analytics.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Analytics />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
